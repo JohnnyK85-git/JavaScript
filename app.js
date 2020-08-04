372705658513a78608de3debf3d79b70f128e5c1
JohnnyK85-git <johnnycbcs@gmail.com>
2020-08-04 20:11:54 +0200

    updated basic JavaScript repository

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const server = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const server = express();
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({
-    extended: false
-}))
+server.use(bodyParser.json());
+server.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
-server.get('/', function (req, res) {
-    res.sendFile('/home/oem/Documents/FbW39_Lessons/01-HTML/01-BASICS/contact.html')
-})
+server.get("/", function (req, res) {
+  res.sendFile(
+    "/home/oem/Documents/FbW39_Lessons/01-HTML/01-BASICS/contact.html"
+  );
+});
 
-server.post('/submit', function (req, res) {
-    console.log(req.body)
-    let response = `<h1>Hi there</h1>
+server.post("/submit", function (req, res) {
+  console.log(req.body);
+  let response = `<h1>Hi there</h1>
       <p>Name: ${req.body.name}</p>
       <p>Email: ${req.body.email}</p>
       <p>Age: ${req.body.age}</p>
       <p>Message: ${req.body.message}</p>
-  `
-    console.log('Working')
-    res.send(response)
-})
+  `;
+  console.log("Working");
+  res.send(response);
+});
 
-const port = 3637
-server.listen(port)
+const port = 3637;
+server.listen(port);
